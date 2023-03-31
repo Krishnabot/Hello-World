edf7e9a5a526527ffd3655ad60f7162a5472de47
Krishna <libraryismyparadise@gmail.com>
2023-03-31 22:44:08 +0545

    Update webpack.config.js to include the babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,16 @@ module.exports = {
   mode: "production",
   devtool: "source-map",
   entry: {
-    application: "./app/javascript/application.js"
+    application: "./app/javascript/application.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
   },
   output: {
     filename: "[name].js",
@@ -14,7 +23,7 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    })
-  ]
-}
+      maxChunks: 1,
+    }),
+  ],
+};
